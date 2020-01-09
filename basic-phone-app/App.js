44a91726e9fa7c7a062a4a3f228a4875164902a9
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-08 23:28:33 -0700

    feat: Add screen 'camera'

--- basic-phone-app/App.js
+++ basic-phone-app/App.js
@@ -1,142 +1,20 @@
-import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, StatusBar, Image, Text, View, TouchableOpacity, SafeAreaView, ScrollView} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.headerContainer}>
-            <Text style={styles.headerText}>Photo App</Text>
-      </View>
-      <ScrollView>
-        <View style={styles.homeContainer}>
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-          <Image
-            style={styles.image}
-            source={{uri: 'http://via.placeholder.com/200x150'}}
-          />
-        </View>
-      </ScrollView>
-      <View style={styles.homeFooter}>
-          <TouchableOpacity style={styles.circleButton}>
-            <Ionicons name="md-camera" size={30}/>
-          </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
+import HomeScreen from './screens/HomeScreen';
+import CameraScreen from './screens/CameraScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
-    flexDirection: 'column',
-    marginTop: StatusBar.currentHeight
-  },
-  headerContainer: {
-    marginTop: 10,
-    padding: 15
-  },
-  headerText: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'left'
-  },
-  homeContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    padding: 10
-  },
-  image: {
-    width: 100,
-    height: 100,
-    margin: 5,
-  },
-  homeFooter: {
-    height: 100,
-    padding: 15,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  circleButton: {
-    borderWidth:2,
-    borderColor: 'black',
-    alignItems:'center',
-    justifyContent:'center',
-    width:75,
-    height:75,
-    backgroundColor:'#fff',
-    borderRadius:150,
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Camera: {screen: CameraScreen},
+},
+{
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    headerShown: false
   }
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
---
+++ basic-phone-app/screens/HomeScreen.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, StatusBar, Image, Text, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+
+export default class HomeScreen extends Component {
+    render() {
+        const {navigate} = this.props.navigation;
+        return (
+            <SafeAreaView style={styles.container}>
+            <View style={styles.headerContainer}>
+                    <Text style={styles.headerText}>Photo App2</Text>
+            </View>
+            <ScrollView>
+                <View style={styles.homeContainer}>
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri: 'http://via.placeholder.com/200x150'}}
+                />
+                </View>
+            </ScrollView>
+            <View style={styles.homeFooter}>
+                <TouchableOpacity style={styles.circleButton} onPress={() => navigate('Camera')}>
+                    <Ionicons name="md-camera" size={30}/>
+                </TouchableOpacity>
+            </View>
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+    marginTop: StatusBar.currentHeight
+},
+headerContainer: {
+    marginTop: 10,
+    padding: 15
+},
+headerText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'left'
+},
+homeContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 10
+},
+image: {
+    width: 100,
+    height: 100,
+    margin: 5,
+},
+homeFooter: {
+    height: 100,
+    padding: 15,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+},
+circleButton: {
+    borderWidth:2,
+    borderColor: 'black',
+    alignItems:'center',
+    justifyContent:'center',
+    width:75,
+    height:75,
+    backgroundColor:'#fff',
+    borderRadius:150,
+}
+
+});
