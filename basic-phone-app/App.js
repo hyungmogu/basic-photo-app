b534992060f141cf2587567a057c41774f95e46a
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-08 18:21:47 -0700

    feat: Add images 'static, home'

--- basic-phone-app/App.js
+++ basic-phone-app/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { StyleSheet, StatusBar, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, StatusBar, Image, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
             <Text style={styles.headerText}>Photo App</Text>
+      </View>
+      <View style={styles.homeContainer}>
+        <Image
+          style={styles.image}
+          source={{uri: 'http://via.placeholder.com/200x150'}}
+        />
+        <Image
+          style={styles.image}
+          source={{uri: 'http://via.placeholder.com/200x150'}}
+        />
+        <Image
+          style={styles.image}
+          source={{uri: 'http://via.placeholder.com/200x150'}}
+        />
+        <Image
+          style={styles.image}
+          source={{uri: 'http://via.placeholder.com/200x150'}}
+        />
+        <Image
+          style={styles.image}
+          source={{uri: 'http://via.placeholder.com/200x150'}}
+        />
+        <Image
+          style={styles.image}
+          source={{uri: 'http://via.placeholder.com/200x150'}}
+        />
       </View>
     </SafeAreaView>
   );
@@ -27,5 +53,17 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'left'
+  },
+  homeContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 10
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 5,
   }
 });
